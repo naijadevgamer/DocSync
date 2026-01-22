a95b10cdb3687e3c43529f74ee7952f91b59ce93
thenaijagamer <sabdullahialaba50@gmail.com>
2026-01-22 21:06:39 +0100

    Configure appwrite

---
+++ src/lib/appwrite.config.ts
@@ -0,0 +1,27 @@
+import * as sdk from "node-appwrite";
+
+const {
+  PROJECT_ID,
+  API_KEY,
+  DATABASE_ID,
+  PATIENT_TABLE_ID,
+  DOCTOR_TABLE_ID,
+  APPOINTMENT_TABLE_ID,
+  NEXT_PUBLIC_BUCKET_ID: BUCKET_ID,
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
+} = process.env;
+
+console.log({ ENDPOINT, PROJECT_ID, API_KEY });
+console.log(typeof ENDPOINT, ENDPOINT);
+
+const client = new sdk.Client();
+
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
+  .setProject(process.env.PROJECT_ID!)
+  .setKey(process.env.API_KEY!);
+
+export const databases = new sdk.Databases(client);
+export const users = new sdk.Users(client);
+export const messaging = new sdk.Messaging(client);
+export const storage = new sdk.Storage(client);
