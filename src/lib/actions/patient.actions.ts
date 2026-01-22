d7135c032e9a29661733da50f69781cc9ad0b2f1
thenaijagamer <sabdullahialaba50@gmail.com>
2026-01-22 21:09:08 +0100

    Setup create user

---
+++ src/lib/actions/patient.actions.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+// const result = await users.create(
+//   "<USER_ID>", // userId
+//   "email@example.com", // email (optional)
+//   "+12065550100", // phone (optional)
+//   "", // password (optional)
+//   "<NAME>", // name (optional)
+// );
+
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    // const newuser = await users.create(
+    //   ID.unique(),
+    //   user.email,
+    //   user.phone,
+    //   undefined,
+    //   user.name,
+    // );
+    const newUser = await users.create({
+      userId: ID.unique(),
+      email: user.email,
+      phone: user.phone,
+      name: user.name,
+      // password: "optional", // only if you want to set one
+    });
+
+    return parseStringify(newUser);
+  } catch (error: any) {
+    // Check existing user
+    if (error && error?.code === 409) {
+      // const existingUser = await users.list([
+      //   Query.equal("email", [user.email]),
+      // ]);
+
+      const existingUser = await users.list({
+        queries: [Query.equal("email", [user.email])],
+      });
+
+      return existingUser.users[0];
+    }
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
