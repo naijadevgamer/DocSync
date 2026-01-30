39e62ed2669666523d93ea376d3c357e3ebfe4b9
thenaijagamer <sabdullahialaba50@gmail.com>
2026-01-30 04:40:27 +0100

    update patient.actions.ts

--- src/lib/actions/patient.actions.ts
+++ src/lib/actions/patient.actions.ts
@@ -46,3 +46,12 @@ export const createUser = async (user: CreateUserParams) => {
     console.error("An error occurred while creating a new user:", error);
   }
 };
+
+export const getUserById = async (userId: string) => {
+  try {
+    const user = await users.get({ userId });
+    return parseStringify(user);
+  } catch (error) {
+    console.error("An error occurred while fetching the user:", error);
+  }
+};
