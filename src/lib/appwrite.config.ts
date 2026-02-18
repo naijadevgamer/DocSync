9ac21f96a0b4445482c825ace5af38dde5eab332
thenaijagamer <sabdullahialaba50@gmail.com>
2026-02-19 00:00:14 +0100

    update appwrite.config.ts

--- src/lib/appwrite.config.ts
+++ src/lib/appwrite.config.ts
@@ -15,7 +15,7 @@ const client = new sdk.Client();
 
 client
   .setEndpoint(process.env.NEXT_PUBLIC_ENDPOINT!)
-  .setProject(process.env.PROJECT_ID!)
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID!)
   .setKey(process.env.API_KEY!);
 
 export const databases = new sdk.Databases(client);
