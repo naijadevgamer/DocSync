378333fd96df043467fa97812eacf7d468acd5c8
thenaijagamer <sabdullahialaba50@gmail.com>
2026-02-18 23:59:32 +0100

    Fix gender bug

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,11 @@
-export const GenderOptions = ["Male", "Female", "Other"];
+export const GenderOptions = ["male", "female", "other"];
 
 export const PatientFormDefaultValues = {
   name: "",
   email: "",
   phone: "",
   birthDate: new Date(Date.now()),
-  gender: "Male" as Gender,
+  gender: "male" as Gender,
   address: "",
   occupation: "",
   emergencyContactName: "",
