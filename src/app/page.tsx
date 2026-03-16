9df5f33700dc30fb9a41e0fe20de4595af51bd48
thenaijagamer <sabdullahialaba50@gmail.com>
2026-03-16 19:16:24 +0100

    update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default async function HomePage({ searchParams }: SearchParamProps) {
           fill
           sizes="(max-width: 768px) 100vw, 50vw"
           className="object-cover"
+          priority
         />
       </div>
     </div>
