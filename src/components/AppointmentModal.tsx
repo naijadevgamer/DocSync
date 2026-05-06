a04a36ad8e9939218bb3340a7503f562404de717
thenaijagamer <sabdullahialaba50@gmail.com>
2026-05-06 23:53:34 +0100

    update AppointmentModal.tsx

--- src/components/AppointmentModal.tsx
+++ src/components/AppointmentModal.tsx
@@ -37,13 +37,13 @@ export const AppointmentModal = ({
       <DialogTrigger asChild>
         <Button
           variant="ghost"
-          className={`capitalize ${type === "schedule" && "text-green-500"}`}
+          className={`capitalize transition ${type === "schedule" && "text-green-500 hover:text-green-400"} ${type === "cancel" && "text-red-500 hover:text-red-400"}`}
         >
           {type}
         </Button>
       </DialogTrigger>
-      <DialogContent className="shad-dialog sm:max-w-md">
-        <DialogHeader className="mb-4 space-y-3">
+      <DialogContent className="shad-dialog max-h-[90vh] overflow-y-auto sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-2 sm:space-y-3">
           <DialogTitle className="capitalize">{type} Appointment</DialogTitle>
           <DialogDescription>
             Please fill in the following details to {type} appointment
