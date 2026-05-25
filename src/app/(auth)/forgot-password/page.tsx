83f934eb435da34f1ddeb59e16446f53cf16f891
thenaijagamer <sabdullahialaba50@gmail.com>
2026-05-25 05:12:23 +0100

    feat: create page.tsx, page.tsx and ForgotPassword.tsx

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,50 @@
+import ForgotPassword from "@/components/forms/ForgotPassword";
+import FullLogo from "@/components/utils/FullLogo";
+import Image from "next/image";
+
+export default async function ForgotPasswordPage() {
+  return (
+    <div className="flex h-screen">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-124">
+          <div className="mb-12">
+            <FullLogo />
+          </div>
+
+          <ForgotPassword />
+
+          <div className="text-14-regular flex justify-between py-12">
+            <p className="text-dark-600 justify-items-end xl:text-left">
+              © {new Date().getFullYear()} DocSync
+            </p>
+          </div>
+        </div>
+      </section>
+
+      <div className="side-img relative h-full w-[50%]">
+        <Image
+          src="/assets/images/onboarding-img.png"
+          alt="patient"
+          fill
+          sizes="(max-width: 768px) 100vw, 50vw"
+          className="object-cover"
+          priority
+        />
+
+        <div className="from-dark-300/80 absolute inset-0 bg-linear-to-t to-transparent" />
+        <div className="absolute right-12 bottom-12 left-12">
+          <blockquote className="space-y-2">
+            <q className="text-18-bold block text-white">
+              DocSync has transformed how we manage patient care. The efficiency
+              gains are remarkable.
+            </q>
+
+            <footer className="text-14-regular text-dark-700">
+              — Dr. Sarah Johnson, Medical Director
+            </footer>
+          </blockquote>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/forms/ForgotPassword.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+// import FullLogo from "@/components/FullLogo";
+// import { sendPasswordRecovery } from "@/lib/actions/auth.actions";
+import { sendPasswordRecovery } from "@/lib/appwrite/actions/auth.actions";
+import { handleActionError } from "@/lib/errors/handle-action-error";
+import { ForgotPasswordFormValidation } from "@/lib/validators/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AlertCircle } from "lucide-react";
+import Link from "next/link";
+import { IoIosMail, IoMdDoneAll } from "react-icons/io";
+import z from "zod";
+import { Alert, AlertDescription } from "../ui/alert";
+import { FieldGroup } from "../ui/field";
+import SubmitButton from "../utils/SubmitButton";
+import CustomFormField, { FormFieldType } from "./util/CustomFormField";
+
+export default function ForgotPassword() {
+  const [submitted, setSubmitted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof ForgotPasswordFormValidation>>({
+    resolver: zodResolver(ForgotPasswordFormValidation),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (
+    data: z.infer<typeof ForgotPasswordFormValidation>,
+  ) => {
+    setIsLoading(true);
+    const res = await sendPasswordRecovery(data.email);
+    if (!res.success) {
+      handleActionError(res.error);
+      return;
+    }
+
+    setSubmitted(true);
+    toast.success("Recovery email sent");
+    setIsLoading(false);
+  };
+
+  if (submitted) {
+    return (
+      <div className="flex-1 content-center justify-center space-y-4 self-center text-center">
+        <p className="flex flex-col justify-center text-center text-green-500">
+          <IoMdDoneAll size={60} className="mx-auto" />
+          <p className="text-2xl"> Email sent!</p>
+        </p>
+        <p className="text-dark-600">Check your inbox.</p>
+        <Button className="shad-primary-btn" asChild>
+          <Link href="/login">Back to Login</Link>
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex-1 space-y-6">
+        <section className="mb-12 space-y-4">
+          <h1 className="text-24-bold mb-2">Forgot Password</h1>
+        </section>
+
+        {form.formState.errors.root && (
+          <Alert className="border-red-500/30 bg-red-500/10 text-red-500">
+            <AlertCircle />
+            <AlertDescription className="text-14-regular text-red-500">
+              {form.formState.errors.root.message}
+            </AlertDescription>
+          </Alert>
+        )}
+
+        <FieldGroup>
+          <CustomFormField
+            fieldType={FormFieldType.INPUT}
+            control={form.control}
+            name="email"
+            label="Email address"
+            placeholder="johndoe@gmail.com"
+            icon={<IoIosMail size={22} />}
+          />
+        </FieldGroup>
+
+        <SubmitButton isLoading={isLoading}>Send Recovery Email</SubmitButton>
+
+        <p className="text-dark-600 text-14-regular mt-4 text-center">
+          Go Back to{" "}
+          <Link href="/login" className="text-green-500 hover:underline">
+            Log in
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+}
