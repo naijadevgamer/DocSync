8230fda7199a3b9e2bd0fe99f1e0ae977ebc5b68
thenaijagamer <sabdullahialaba50@gmail.com>
2026-06-02 07:58:28 +0100

    Fix build error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 // middleware.ts
-import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { createSessionClientFromMiddleware } from "./lib/appwrite/config/server";
+import { NextResponse } from "next/server";
 import { verifyToken } from "./lib/appwrite/helper/jwt";
 
 export async function middleware(request: NextRequest) {
